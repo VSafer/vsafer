ff4670698eea522096c237fec83efaf7e1517ec9
npmania <np@mkv.li>
2024-04-03 06:59:31 +0900

    crossexservice: duplicated with touchennxkey

--- vsafer/scan/targets/__init__.py
+++ vsafer/scan/targets/__init__.py
@@ -3,7 +3,6 @@ from typing import List
 from vsafer.types import ScanTarget
 
 from vsafer.scan.targets.common import common_targets
-import vsafer.scan.targets.crossexservice as crossexservice
 import vsafer.scan.targets.dext5upload as dext5upload
 import vsafer.scan.targets.ztransferx as ztransferx
 import vsafer.scan.targets.crownix as crownix
@@ -12,8 +11,6 @@ import vsafer.scan.targets.ozweblauncher as ozweblauncher
 
 
 targets: List[ScanTarget] = common_targets
-if crossexservice.target is not None:
-    targets.append(crossexservice.target)
 targets.append(dext5upload.target)
 targets += ztransferx.targets
 targets.append(crownix.target)
